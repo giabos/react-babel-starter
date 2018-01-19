a31254a6389926f0578835d91eb14088549a7437
giovanni <giovanni.boscarino@xenit.eu>
2018-01-19 16:05:54 +0100

    initial release

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import  React from 'react';
+import  {Component} from 'react';
+import { render } from 'react-dom';
+
+
+render(<div>hello</div>, document.getElementById('react-root'));
+
+
+
+
+console.log("hello world");
+
+
