89f44bf06f0fe4271ddf676995a4c9576b9c0de8
giovanni <giovanni.boscarino@xenit.eu>
2018-01-19 16:20:05 +0100

    same with parcel bundler

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import  React from 'react';
 import  {Component} from 'react';
 import { render } from 'react-dom';
 
-
+debugger;
 render(<div>hello</div>, document.getElementById('react-root'));
 
 
